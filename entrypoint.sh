a64e00e088b87b8b3d8fb1ffccc1408a88b39f66
Essigautomat <mw1996oede@googlemail.com>
2019-10-09 23:39:29 +0200

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-if [ "$1" == "zip" ]
+if [ "$1" == "zip" ]; then
 	zip -r $2 ${@:3}
-elif [ "$1" == "7z" ]
+elif [ "$1" == "7z" ]; then
 	7z a $2 ${@:3}
-elif [ "$1" == "tar" ]
+elif [ "$1" == "tar" ]; then
 	tar -czf $2 ${@:3}
-elif [ "$1" == "bzip2" ]
+elif [ "$1" == "bzip2" ]; then
 	tar -cjf $2 ${@:3}
 fi
