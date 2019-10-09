2ab1266458487207afc468a22289e9c2ebd7ba3f
Essigautomat <mw1996oede@googlemail.com>
2019-10-09 23:52:39 +0200

    now using bash extensions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
-if [ "$1" == "zip" ]; then
+if [[ "$1" == "zip" ]]; then
 	zip -r $2 ${@:3}
-elif [ "$1" == "7z" ]; then
+elif [[ "$1" == "7z" ]]; then
 	7z a $2 ${@:3}
-elif [ "$1" == "tar" ]; then
+elif [[ "$1" == "tar" ]]; then
 	tar -czf $2 ${@:3}
-elif [ "$1" == "bzip2" ]; then
+elif [[ "$1" == "bzip2" ]]; then
 	tar -cjf $2 ${@:3}
 fi
