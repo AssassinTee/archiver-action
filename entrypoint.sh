23da860b821a5063829816686c2bc04c3374a1db
Holger Karwanni <holger.karwanni@smail.inf.h-brs.de>
2019-10-10 13:01:40 +0200

    changed completly to env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [[ "$1" == "check" ]]; then
-        echo $@
+if [[ "$INPUT_ARCHIVER" == "debug" ]]; then
+        echo "$INPUT_ARCHIVER"
         echo "$INPUT_FILES"
         echo "$INPUT_NAME"
-elif [[ "$1" == "zip" ]]; then
+elif [[ "$INPUT_ARCHIVER" == "zip" ]]; then
 	zip -r $INPUT_NAME $INPUT_FILES
-
-elif [[ "$1" == "7z" ]]; then
-	7z a $2 ${@:3}
-elif [[ "$1" == "tar" ]]; then
-	tar -czf $2 ${@:3}
-elif [[ "$1" == "bzip2" ]]; then
-	tar -cjf $2 ${@:3}
+elif [[ "$INPUT_ARCHIVER" == "7z" ]]; then
+	7z a $INPUT_NAME $INPUT_FILES
+elif [[ "$INPUT_ARCHIVER" == "tar" ]]; then
+	tar -czf $INPUT_NAME $INPUT_FILES
+elif [[ "$INPUT_ARCHIVER" == "bzip2" ]]; then
+	tar -cjf $INPUT_NAME $INPUT_FILES
+else
+        echo "unkown archiver: $INPUT_ARCHIVER"
+        exit 1
 fi
