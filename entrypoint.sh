c28a40297a224bd124ec6122ad635b9487100eca
Holger Karwanni <holger.karwanni@smail.inf.h-brs.de>
2019-10-10 12:51:52 +0200

    now using environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ if [[ "$1" == "check" ]]; then
         echo "$INPUT_FILES"
         echo "$INPUT_NAME"
 elif [[ "$1" == "zip" ]]; then
-	zip -r $2 ${@:3}
+	zip -r $INPUT_NAME $INPUT_FILES
+
 elif [[ "$1" == "7z" ]]; then
 	7z a $2 ${@:3}
 elif [[ "$1" == "tar" ]]; then
